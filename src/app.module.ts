42a3e8455846a16308ed513dc3e10b7b09adb4b5
ShawnTseng <shawntseng40@gmail.com>
2023-09-26 23:25:23 +0800

    fix: npm run start failed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { PublicService } from './service/public.service';
 import { AccountController } from './controller/account.controller';
 import { BlockController } from './controller/block.controller';
 import { FriendTechController } from './controller/friend-tech/friend-tech.controller';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaService } from './service/prisma/prisma.service';
+import { PrismaService } from './service/prisma.service';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   controllers: [
     AppController,
     AccountController,
